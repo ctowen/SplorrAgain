31bb0927dbd9c0981e754c10cc9edf812bb2d005
ctowen <craig.t.owen@intel.com>
2016-07-29 14:15:34 -0700

    Initial Demo

---
+++ TangoTest2/Assets/TouchButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchButton : MonoBehaviour
+{
+    public Color defaultColor;
+    public Color selectedColor;
+    private Material mat;
+
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        mat = rend.material;
+    }
+
+    void OnTouchDown()
+    {
+        mat.color = selectedColor;
+        gameObject.AddComponent<SendWWWMessage>();
+    }
+    void OnTouchUp()
+    {
+        mat.color = defaultColor;
+    }
+    void OnTouchStay()
+    {
+        mat.color = selectedColor;
+    }
+    void OnTouchExit()
+    {
+        mat.color = defaultColor;
+    }
+}
+
+public class SendWWWMessage : MonoBehaviour
+{
+    public void Start() 
+    {
+        string url = "http://10.0.69.111:1881/test/";
+        byte[] postBytes = System.Text.Encoding.ASCII.GetBytes("on");
+        WWW www = new WWW(url, postBytes);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        if (www.error == null)
+            Debug.Log("WWW Success: " + www.text + " - " + www.url);
+        else
+            Debug.Log("WWW Error: " + www.error + " - " + www.url);
+    }
+}
